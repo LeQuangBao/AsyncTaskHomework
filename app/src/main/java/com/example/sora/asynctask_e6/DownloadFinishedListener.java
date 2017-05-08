27b248ed6712fc868d406af53a38be51a012579b
Lê Quang Bảo <lequangbao13351@gmail.com>
2017-05-08 17:14:06 +0700

    first commit

---
+++ app/src/main/java/com/example/sora/asynctask_e6/DownloadFinishedListener.java
@@ -0,0 +1,5 @@
+package com.example.sora.asynctask_e6;
+
+interface DownloadFinishedListener {
+	void notifyDataRefreshed(String[] feeds);
+}
---
+++ app/src/main/java/com/example/sora/asynctask_e6/SelectionListener.java
@@ -0,0 +1,6 @@
+package com.example.sora.asynctask_e6;
+
+interface SelectionListener {
+	public void onItemSelected(int position);
+	public boolean canAllowUserClicks();
+}
